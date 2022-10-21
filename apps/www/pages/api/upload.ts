212d8cb35e2b8105a5431c468f18cd7401d20e57
so2liu <yangliu35@outlook.com>
2022-10-21 14:22:29 +0800

    Revert "Revert "feat: 使用腾讯云来管理上传图片等文件""

--- apps/www/pages/api/upload.ts
+++ apps/www/pages/api/upload.ts
@@ -1,26 +1,41 @@
-import aws from 'aws-sdk'
+import tcb from '@cloudbase/node-sdk'
+import { NextApiRequest, NextApiResponse } from 'next'
+import formidable from 'formidable'
+import { createReadStream } from 'fs'
 
-export default async function handler(req, res) {
-  aws.config.update({
-    accessKeyId: process.env.TL_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.TL_AWS_SECRET_KEY,
-    region: process.env.TL_AWS_REGION,
-    signatureVersion: 'v4',
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const app = tcb.init({
+  secretId: process.env.TCL_PRIVATE_ID,
+  secretKey: process.env.TCL_PRIVATE_KEY,
+  env: process.env.TCL_ENV_ID,
+})
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const data = await new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
+    (res, rej) => {
+      const form = formidable({})
+      form.parse(req, (err, fields, files) => {
+        if (err) rej(err)
+        else res({ fields, files })
+      })
+    }
+  )
+
+  const file = data.files.myFile[0]
+  const result = await app.uploadFile({
+    cloudPath: `upload/${file.newFilename}_${file.originalFilename}`,
+    fileContent: createReadStream(file.filepath),
   })
 
-  const s3 = new aws.S3()
-
-  const post = s3.createPresignedPost({
-    Bucket: process.env.TL_AWS_BUCKET_NAME,
-    Fields: {
-      key: req.query.file,
-      'Content-Type': req.query.fileType,
-    },
-    Expires: 60, // seconds
-    Conditions: [
-      ['content-length-range', 0, 5242880], // up to 5 MB
-    ],
+  const url = await app.getTempFileURL({
+    fileList: [result.fileID],
   })
+  res.send(url.fileList[0])
 
-  res.status(200).json(post)
+  return
 }
